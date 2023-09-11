fd9a5b16878c1066466b04cbc767ecc73fd4b6ae
Sopan Bhere <sbussiness21@gmail.com>
2023-09-11 20:10:39 +0530

    data type in javascript code added

---
+++ DataTypes.js
@@ -0,0 +1,45 @@
+// Data types
+/*
+Primitive :
+1. String type
+2. Number type
+3. Boolean Type 
+4. Undefined Type
+5. Null Type
+6. Symbol type
+7. Bigint Type
+
+Non primitive
+1. Object
+*/
+
+// string data type
+const name = 'Sopan'
+const language = "JavaScript"
+console.log(typeof(name))
+console.log(name)
+
+
+// number data type
+const age = 19
+const employee_salary = 29.56
+console.log(typeof(age))
+console.log(age)
+console.log(typeof(employee_salary))
+console.log(employee_salary)
+
+// Boolean data type 
+const isPrimery = true
+const isNewUser = false
+console.log(typeof(isPrimery))
+console.log(isPrimery)
+
+// undefined data type
+let result
+console.log(typeof(result))
+console.log(result)
+
+// Null data type
+const data = null
+console.log(typeof(data))
+console.log(data)
