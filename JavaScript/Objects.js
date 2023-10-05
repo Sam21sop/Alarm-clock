dcf9df2a66c21b4552b242377110b7cddba22afe
Sam21sop <sbussiness21@gmail.com>
2023-10-05 13:42:58 +0530

    first html page.

--- JavaScript/Objects.js
+++
@@ -1,6 +0,0 @@
-const person = {
-    'first_name'  : 'Sopan',
-    'last_name' : 'Bhere'}
-
-console.log(person.first_name)
-console.log(person.last_name)
--- JavaScript/variables.js
+++
@@ -1,6 +0,0 @@
-let age = 25
-console.log(age)
-
-
-const salary = 2000
-console.log(salary)
